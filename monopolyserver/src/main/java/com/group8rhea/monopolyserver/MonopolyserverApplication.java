b0a19bf75496af0148c10c00edf28437206c6535
Gorkem Karaduman <gorkemkrdmn@deda.wls.metu.edu.tr>
2022-03-31 21:21:03 +0300

    Add mariadb dependecy and disable datasoure
    - Home url runs successfully and returns Hello World!

--- monopolyserver/src/main/java/com/group8rhea/monopolyserver/MonopolyserverApplication.java
+++ monopolyserver/src/main/java/com/group8rhea/monopolyserver/MonopolyserverApplication.java
@@ -1,10 +1,21 @@
 package com.group8rhea.monopolyserver;
 
+//import java.lang.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@SpringBootApplication
+@Controller
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class MonopolyserverApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MonopolyserverApplication.class, args);
