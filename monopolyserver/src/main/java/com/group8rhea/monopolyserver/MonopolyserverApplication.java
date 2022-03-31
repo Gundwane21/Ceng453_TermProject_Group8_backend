970346d175c8df89cf0a53f704ea575edfd99772
Kerem Serttas <keremserttas21@gmail.com>
2022-03-31 20:29:16 +0300

    Add spring initiliazer files

---
+++ monopolyserver/src/main/java/com/group8rhea/monopolyserver/MonopolyserverApplication.java
@@ -0,0 +1,13 @@
+package com.group8rhea.monopolyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonopolyserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonopolyserverApplication.class, args);
+	}
+
+}
---
+++ monopolyserver/src/test/java/com/group8rhea/monopolyserver/MonopolyserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group8rhea.monopolyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonopolyserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
