0d4eaa2239e6b210611c9c3135905020243732bf
Kerem Serttas <keremserttas21@gmail.com>
2022-04-17 12:50:35 +0300

    Remove spring security filters for registration setup

--- monopolyserver/src/main/java/com/group8rhea/monopolyserver/configs/WebSecurityConfig.java
+++ monopolyserver/src/main/java/com/group8rhea/monopolyserver/configs/WebSecurityConfig.java
@@ -23,14 +23,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .csrf()
-                .disable()
-                .authorizeRequests()
-                .antMatchers("/registration")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
+                .disable();
+//                .authorizeRequests()
+//                .antMatchers("/api/register")
+//                .permitAll()
+//                .anyRequest()
+//                .authenticated()
+//                .and();
+//                .httpBasic();
     }
 
     @Bean
