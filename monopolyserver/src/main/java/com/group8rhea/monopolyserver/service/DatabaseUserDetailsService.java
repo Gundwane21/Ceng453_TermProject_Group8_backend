4cc3ecdfcd7818aaa6d5c09964c92c46946c0767
Kerem Serttas <keremserttas21@gmail.com>
2022-04-17 12:52:20 +0300

    Add service to add users to db

--- monopolyserver/src/main/java/com/group8rhea/monopolyserver/service/DatabaseUserDetailsService.java
+++ monopolyserver/src/main/java/com/group8rhea/monopolyserver/service/DatabaseUserDetailsService.java
@@ -1,5 +1,6 @@
 package com.group8rhea.monopolyserver.service;
 
+import com.group8rhea.monopolyserver.dto.UserDto;
 import com.group8rhea.monopolyserver.model.UserModel;
 import com.group8rhea.monopolyserver.repository.UserModelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,8 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -19,6 +22,7 @@ public class DatabaseUserDetailsService  implements UserDetailsService {
 
     @Autowired
     private UserModelRepository userModelRepository;
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -39,5 +43,12 @@ public class DatabaseUserDetailsService  implements UserDetailsService {
                     .build();
             return user;
         }
+    }
+
+    public String registerUser(UserDto userDto){
+        UserModel userModel = new UserModel(userDto.getUsername()
+                                ,passwordEncoder.encode(userDto.getPassword()));
+        userModelRepository.save(userModel);
+        return new String("Succesfull add to repository");
     }
 }
