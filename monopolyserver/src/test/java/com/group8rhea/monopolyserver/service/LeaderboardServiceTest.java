c8fbf621c9fc854c5c2ea226c285ece284a02061
Gorkem Karaduman <gorkemkrdmn@hotmail.com>
2022-04-22 20:51:48 +0300

    Implement leaderboard service test

--- monopolyserver/src/test/java/com/group8rhea/monopolyserver/service/LeaderboardServiceTest.java
+++ monopolyserver/src/test/java/com/group8rhea/monopolyserver/service/LeaderboardServiceTest.java
@@ -1,11 +1,13 @@
 package com.group8rhea.monopolyserver.service;
 
+import com.group8rhea.monopolyserver.model.TimeInterval;
 import com.group8rhea.monopolyserver.repository.GameRecordStoreRepository;
+import org.junit.Assert;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,7 +16,7 @@ import java.util.Map;
 
 import static org.mockito.Mockito.when;
 
-
+@SpringBootTest
 public class LeaderboardServiceTest {
 
     @Autowired
@@ -24,22 +26,20 @@ public class LeaderboardServiceTest {
     GameRecordStoreRepository gameRecordStoreRepository;
 
     @Test
+    @DisplayName("Test retrieving leaderboard for All Times")
     public void getLeaderboardAllTimesTest() {
-        Map<String, Integer> hashMap = new HashMap<String, Integer>();
         List<Map<String, Long>> result = new ArrayList<>();
         Map<String, Long> map = new HashMap<String, Long>();
-        map.put("Kerem", (long) 100);
-        map.put("Gorkem", (long) 130);
-        map.put("Jonathan", (long) 80);
-        map.put("Melis", (long) 1463);
-        result.add(map);
+        map.put("KeremTheMonopolyKing", (long) 100);
         Map<String, Long> map2 = new HashMap<String, Long>();
-        map2.put("Player1", (long) 18);
-        map2.put("Player2", (long) 17);
-        map2.put("Player3", (long) 9);
+        map2.put("GorkemOfRivia", (long) 180);
+        Map<String, Long> map3 = new HashMap<String, Long>();
+        map2.put("John", (long) 360);
+        result.add(map);
         result.add(map2);
-//        when(gameRecordStoreRepository.getLeaderboardAllTimes()).thenReturn(result);
-//        ResponseDTO returnedValue = (ResponseDTO) scoreBoardService.getScoreBoard(hashMap).getSecond();
-//        Assert.assertEquals(result, returnedValue.getData());
+        result.add(map3);
+        when(gameRecordStoreRepository.getLeaderboardAllTimes()).thenReturn(result);
+        List<Map<String, Long>> leaderboardAllTimes = leaderboardService.getLeaderboard(TimeInterval.ALL_TIMES);
+        Assert.assertEquals(result, leaderboardAllTimes);
     }
 }
