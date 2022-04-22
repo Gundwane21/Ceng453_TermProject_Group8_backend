d84b8488cac20514e9e57593efce56d5704e2681
Gorkem Karaduman <gorkemkrdmn@hotmail.com>
2022-04-22 20:51:27 +0300

    Add Javadoc to JPA repositories for game_record and game_score tables

--- monopolyserver/src/main/java/com/group8rhea/monopolyserver/repository/GameRecordModelRepository.java
+++ monopolyserver/src/main/java/com/group8rhea/monopolyserver/repository/GameRecordModelRepository.java
@@ -4,6 +4,9 @@ import com.group8rhea.monopolyserver.model.GameRecordEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * This interface handles storing games into game_record table.
+ */
 @Repository
 public interface GameRecordModelRepository extends CrudRepository<GameRecordEntity, Integer> {
 
--- monopolyserver/src/main/java/com/group8rhea/monopolyserver/repository/GameRecordStoreRepository.java
+++ monopolyserver/src/main/java/com/group8rhea/monopolyserver/repository/GameRecordStoreRepository.java
@@ -4,14 +4,23 @@ import com.group8rhea.monopolyserver.model.GameRecordScoreEntity;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+/**
+ * This interface handles the queries on the joined table from game_record and game_score. It handles retrieval of the leaderboard.
+ */
+@Repository
 public interface GameRecordStoreRepository extends CrudRepository<GameRecordScoreEntity, UUID> {
 
+
+    /**
+     * @return Returns the leaderboard considering all the games.
+     */
     @Query(value =
             "SELECT um.username, ids.score " +
             "FROM ( " +
@@ -20,8 +29,13 @@ public interface GameRecordStoreRepository extends CrudRepository<GameRecordScor
                     "WHERE gs.game_id = gr.game_id " +
                     "GROUP BY gs.user_id ) ids, user_model um " +
             "WHERE um.id = ids.id;", nativeQuery = true)
-    List<Map<String, Integer>> getLeaderboardAllTimes();
+    List<Map<String, Long>> getLeaderboardAllTimes();
 
+    /**
+     * @param startDate Determines the start date of the game to be considered.
+     * @param endDate Determines the end date of the game to be considered.
+     * @return Returns the leaderboard considering games whose dates are between startDate and endDate.
+     */
     @Query(value =
             "SELECT um.username, ids.score " +
                     "FROM ( " +
@@ -30,5 +44,5 @@ public interface GameRecordStoreRepository extends CrudRepository<GameRecordScor
                     "WHERE gs.game_id = gr.game_id and gr.created_at >= :startDate and gr.created_at <= :endDate " +
                     "GROUP BY gs.user_id ) ids, user_model um " +
                     "WHERE um.id = ids.id;", nativeQuery = true)
-    List<Map<String, Integer>> getLeaderboardBetweenDates(@Param("startDate") Date startDate, @Param("endDate") Date endDate);
+    List<Map<String, Long>> getLeaderboardBetweenDates(@Param("startDate") Date startDate, @Param("endDate") Date endDate);
 }
