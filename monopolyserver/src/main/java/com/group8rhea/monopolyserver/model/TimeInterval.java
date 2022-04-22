6bc340c2ab3a15b188a4437593088837b2319a10
Gorkem Karaduman <gorkemkrdmn@hotmail.com>
2022-04-22 20:52:06 +0300

    Add Javadoc comments to TimeInterval enum

--- monopolyserver/src/main/java/com/group8rhea/monopolyserver/model/TimeInterval.java
+++ monopolyserver/src/main/java/com/group8rhea/monopolyserver/model/TimeInterval.java
@@ -1,5 +1,11 @@
 package com.group8rhea.monopolyserver.model;
 
+/**
+ * Represents time intervals for the game dates.
+ * ALL_TIMES: Games played on any date
+ * LAST_MONTH: Games played between today and one month before today
+ * LAST_WEEK: Games played between today and one week before today
+ */
 public enum TimeInterval {
     ALL_TIMES, LAST_MONTH, LAST_WEEK
 }
